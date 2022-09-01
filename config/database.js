bc5a41132564a6037a1fe1f337745b75ae31030b
stfxcr <mwinfratech@gmail.com>
2022-09-01 22:37:51 +0530

    Thlak l;eh chhin

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '198.54.116.86'),
       port: env.int('DATABASE_PORT', 21098),
-      database: env('DATABASE_NAME', 'mizoitme_kethi'),
-      user: env('DATABASE_USERNAME', 'mizoitme_rin'),
+      database: env('DATABASE_NAME', 'aiprrmoh_kethi'),
+      user: env('DATABASE_USERNAME', 'aiprrmoh_rin'),
       password: env('DATABASE_PASSWORD', 'Keimah&*(987#@!'),
       ssl: env.bool('DATABASE_SSL', false),
     },
