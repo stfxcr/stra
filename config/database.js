4601266176b6076980eb526dde607426032b0c14
stfxcr <mwinfratech@gmail.com>
2022-09-05 01:50:35 +0530

    db added

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'ls-0960b3d63ce3dff9561899490952b04e317332dd.chhdowwo4yea.ap-south-1.rds.amazonaws.com'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'mizoitme_kethi'),
-      user: env('DATABASE_USERNAME', 'mizoitme_rin'),
-      password: env('DATABASE_PASSWORD', 'Keimah&*(987#@!'),
+      database: env('DATABASE_NAME', 'dbbereka'),
+      user: env('DATABASE_USERNAME', 'kethi'),
+      password: env('DATABASE_PASSWORD', 'Keimah(*&321'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
